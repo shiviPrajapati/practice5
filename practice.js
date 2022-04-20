347f21521df477bd7b69ab57eeb497a3f27aacfe
shivi <shivi.prajapati.19.1@mountblue.tech>
2022-04-20 21:42:22 +0530

    add reject in append promisification

--- practice.js
+++ practice.js
@@ -1,8 +1,11 @@
 // Q.1  Promisify appendFile function
 const fs = require('fs')
 function test(){
-    return new Promise((res) => {
-        fs.appendFile('./sample.txt', " file appended", ()=> {
+    return new Promise((res,rej) => {
+        fs.appendFile('./sample.txt', 'file appended' , function(err) {
+            if(err){
+                rej(err)
+            }
             res()
         })
     })
@@ -10,7 +13,7 @@ function test(){
 
 test().then((res) => {
     console.log("file appened")
-})
+}).catch((err) => console.log("error"))
 
 
 
