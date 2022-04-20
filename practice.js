afc4bc3e07a7eb5adbdb06e2defc2b22d89c6e14
shivi <shivi.prajapati.19.1@mountblue.tech>
2022-04-20 20:42:04 +0530

    first commit

---
+++ practice.js
@@ -0,0 +1,30 @@
+// Q.1  Promisify appendFile function
+const fs = require('fs')
+function test(){
+    return new Promise((res) => {
+        fs.appendFile('./sample.txt', " file appended", ()=> {
+            res()
+        })
+    })
+}
+
+test().then((res) => {
+    console.log("file appened")
+})
+
+
+
+// Q2 
+// function A = () => Promise.resolve({})
+// function B = () => Promise.resolve({})
+
+// Call A and B together.
+
+
+const A = () => Promise.resolve({})
+const B = () => Promise.resolve({})
+
+
+
+Promise.all([A(), B()]).then(() => {console.log("promise resolved")})
+.catch((rej) => {console.log("Any promise rejected")})
